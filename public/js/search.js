5ef9b8b72db5be18388b9ed4cb0e8df55c53f1ae
SWarrior97 <32170183+SWarrior97@users.noreply.github.com>
2019-05-24 20:54:14 +0100

    falta inserir na BD

--- public/js/search.js
+++ public/js/search.js
@@ -17,9 +17,27 @@ $(document).ready(function () {
         var html = document.getElementById("prencher").innerHTML;
         var splitedData = html.split("[");
         //console.log(splitedData[1]);
-        //console.log(typeof splitedData[1])
+        //console.log(typeof splitedData[1]);
+        var splitted = splitedData[1].split("}");
+        //console.log(splitted);
+
+        for (var i = 0; i < splitted.length; i++) {
+            //console.log(i+" "+splitted[i]+"\n");
+            var data = splitted[i].split('"');
+            //console.log(teste);
+            var numero = data[3];
+            var nome = data[7];
+            var ilha = data[11];
+            var concelho = data[15];
+            console.log("Numero:"+numero+"\n"+
+                        "nome:"+nome+"\n"+
+                        "ilha:"+ilha+"\n"+
+                        "concelho:"+concelho+"\n");
+        }
+
         var jsonData = JSON.parse(splitedData[1]);
-        //console.log(jsonData);
+
+        //console.log(typeof jsonData);
         
     
     }).fail(function(jqXHR, textStatus ) {
@@ -38,9 +56,6 @@ $(document).ready(function () {
         
         }).done(function(resposta) {
             //console.log(resposta);
-            //$('#prencher_empresa').html(resposta);
-            /*var x = document.getElementsByClassName("listing__name--link jsListingName");
-            console.log(x);*/
         }).fail(function(jqXHR, textStatus ) {
             //console.log("Request failed: " + textStatus);
         });
